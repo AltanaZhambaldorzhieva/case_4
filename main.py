b30cecc569563229ba876a1d86c2fa054af6bf0e
daryazaitseva <zaytsevaart@gmail.com>
2024-03-20 00:48:02 +0700

    changes

--- main.py
+++ main.py
@@ -25,17 +25,89 @@ def flowers():
 
 
 def desserts():
-    biscuit_cake = 2000
-    curd_cake = 1800
-    waffel_cake = 1500
-    chocolate_filling = 500
-    berry_filling = 800
-    cream_filling = 650
-    decoration_flowers = 750
-    decoration_berry = 800
-    decoration_chocolate = 600
+    napoleon = 4500
+    cheesecake = 3600
+    tiramisu = 2850
+    prague = 1980
+    red_velvet = 999
+
+    chocolate_capcake = 3999
+    vanila_capcake = 2899
+    coffee_capcake = 1200
+
+    strawberry = 5200
+    coconut = 4150
+    caramel = 3499
+    chocolate = 1200
+
+    sum = 0
     money = int(input(ru.MONEY))
-    print(f'{ru.CATEGORY}: \n1.{ru.CAKES}\n2.{ru.CAPCAKES}\n3.{ru.CANDIES}')
+    print(f'{ru.DESSERT_CATEGORY}: \n1.{ru.CAKES}\n2.{ru.CAPCAKES}\n3.{ru.CANDIES}')
+    category = int(input(ru.CATEGORY))
+    if category == 1:
+        if money > 5_000:
+            print(f'{ru.BUDGET}:\n1.{ru.NAPOLEON}\n2.{ru.CHEESECAKE}\n3.{ru.TIRAMISU}'
+                  f'\n4.{ru.PRAGUE}\n5.{RED_VELVET}')
+        elif money > 4_000:
+            print(f'{ru.BUDGET}:\n2.{ru.CHEESECAKE}\n3.{ru.TIRAMISU}'
+                  f'\n4.{ru.PRAGUE}\n5.{RED_VELVET}')
+        elif money > 3_000:
+            print(f'{ru.BUDGET}:\n3.{ru.TIRAMISU}'
+                  f'\n4.{ru.PRAGUE}\n5.{RED_VELVET}')
+        elif money > 2_000:
+            print(f'{ru.BUDGET}:\n4.{ru.PRAGUE}\n5.{RED_VELVET}')
+        else:
+            print(f'{ru.BUDGET}:\n5.{RED_VELVET}')
+        cake = int(input(ru.DESSERT_CATEGORY))
+        if cake == 1:
+            sum = napoleon
+        elif cake == 2:
+            sum = cheesecake
+        elif cake == 3:
+            sum = tiramisu
+        elif cake == 4:
+            sum = prague
+        else:
+            sum = red_velvet
+
+    elif category == 2:
+        if money > 4_000:
+            print(f'{ru.BUDGET}:\n1.{ru.CHOCOLATE_CAPCAKE}\n2.{ru.VANILA_CAPCAKE}'
+                  f'\n3.{ru.COFFEE_CAPCAKE}')
+        elif money > 3_000:
+            print(f'{ru.BUDGET}:\n2.{ru.VANILA_CAPCAKE}'
+                  f'\n3.{ru.COFFEE_CAPCAKE}')
+        elif money > 1_500:
+            print(f'{ru.BUDGET}:\n3.{ru.COFFEE_CAPCAKE}')
+        capcake = int(input(ru.DESSERT_CATEGORY))
+        if capcake == 1:
+            sum = chocolate_capcake
+        elif capcake == 2:
+            sum = vanila_capcake
+        else:
+            sum = coffee_capcake
+
+    else:
+        if money > 5_000:
+            print(f'{ru.BUDGET}:\n1.{ru.STRAWBERRY}\n2.{ru.COCONUT}'\n3.{ru.CARAMEL}'
+                  f'\n4.{ru.CHOCOLATE}')
+        elif money > 4_000:
+            print(f'{ru.BUDGET}:\n2.{ru.COCONUT}'\n3.{ru.CARAMEL}'
+                  f'\n4.{ru.CHOCOLATE}')
+        elif money > 3_000:
+            print(f'{ru.BUDGET}:\n3.{ru.CARAMEL}'
+                  f'\n4.{ru.CHOCOLATE}')
+        else:
+            print(f'{ru.BUDGET}:\n4.{ru.CHOCOLATE}')
+        candies = int(input(ru.DESSERT_CATEGORY))
+        if candies == 1:
+            sum = strawberry
+        elif candies == 2:
+            sum = coconut
+        elif candies == 3:
+            sum = caramel
+        else:
+            sum = chocolate
 
 
 def jewelry():
