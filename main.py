5a3a956d5b408ac72b2ec04ed08cf2ce1a04d34f
Darialr <darial.ryaguzova@gmail.com>
2024-03-19 21:36:48 +0700

    flower

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ import ru_local as ru
 
 def desire(present):
     return present == ru.ENGAGEMENT_RING
+
+
+def flowers():
+    money = int(input('На какую сумму вы бы хотели приобрести букет?'))
+    if money > 13_000:
+        print('Возможные размеры букетов, согласно вашему бюджету: S, M, L, XL')
+    elif money > 9_000:
+        print('Возможные размеры букетов, согласно вашему бюджету: S, M, L')
+    elif money > 5_000:
+        print('Возможные размеры букетов, согласно вашему бюджету: S, M')
+    else:
+        print('Возможные размеры букетов, согласно вашему бюджету: S')
+    size = input('Выберете размер букета')
